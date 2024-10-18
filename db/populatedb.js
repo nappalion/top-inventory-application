8637167d5d39ac40673908df132d742834325a49
nipplelion <piolotorrico@yahoo.com>
2024-10-17 17:14:02 -0700

    feat: add script to populate database

---
+++ db/populatedb.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+require("dotenv").config();
+const { Client } = require("pg");
+
+const SQL = `
+
+CREATE TABLE IF NOT EXISTS categories (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS items (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255) NOT NULL,
+  quantity INT NOT NULL DEFAULT 0,
+  image_data BYTEA,
+  category_id INT,
+  CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id)
+)
+
+INSERT INTO categories (name)
+VALUES 
+('Category 1'),
+('Category 2'),
+('Category 3'),
+('Category 4')
+
+INSERT INTO items (name, quantity, category_id)
+VALUES 
+('bob', 31, 1),
+('tom', 14, 2),
+('carl', 24, 2),
+('jerry', 68, 3)
+
+INSERT INTO items (name, quantity)
+VALUES 
+('billy', 1)
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    // ssl: { rejectUnauthorized: false },
+    // connectionString: process.env.CONNECTION_STRING,
+    ssl: false,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
