5c89a697e5b468f64f52de73e22ac59c424cdd74
NippleLion <piolotorrico@yahoo.com>
2024-10-19 16:47:58 -0700

    feat: add validation for create and update item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,15 +1,29 @@
 const db = require("../db/queries");
 const crypto = require("crypto");
 const firebaseStorage = require("../firebaseStorage");
+const { body, validationResult } = require("express-validator");
 
-async function createGet(req, res) {
-  const categories = await db.getAllCategories();
+const lengthErr = "must be between 1 and 30 characters.";
+const intErr = "must be a valid integer.";
+const imageErr = "must be a valid file (jpg, jpeg, png, gif).";
 
-  res.render("create-item", {
-    title: "Create New Item",
-    categories: categories,
-  });
-}
+const validateItem = [
+  body("name")
+    .trim()
+    .isLength({ min: 1, max: 30 })
+    .withMessage(`Name ${lengthErr}`),
+  body("quantity").trim().isInt().withMessage(`Quantity ${intErr}`),
+  body("image_data")
+    .optional()
+    .trim()
+    .matches(/\.(jpg|jpeg|png|gif)$/i)
+    .withMessage(`Photo ${imageErr}`),
+  body("category_id")
+    .optional()
+    .trim()
+    .isInt()
+    .withMessage(`Category ID ${intErr}`),
+];
 
 async function uploadFile(fileBuffer) {
   if (!fileBuffer) {
@@ -28,27 +42,50 @@ async function uploadFile(fileBuffer) {
   return fileUrl;
 }
 
-async function createPost(req, res) {
-  const { name, quantity, category_id } = req.body;
-  const new_category_id = parseInt(category_id);
+async function createGet(req, res) {
+  const categories = await db.getAllCategories();
 
-  let fileUrl;
-  if (req.file) {
-    const fileBuffer = req.file.buffer;
-    fileUrl = await uploadFile(fileBuffer);
-  }
-
-  const itemData = {
-    name: name,
-    quantity: quantity,
-    image_data: fileUrl,
-    category_id: new_category_id,
-  };
-
-  await db.createItem(itemData);
-
-  res.redirect(`/`);
+  res.render("create-item", {
+    title: "Create New Item",
+    categories: categories,
+  });
 }
+
+const createPost = [
+  validateItem,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const categories = await db.getAllCategories();
+      return res.status(400).render("create-item", {
+        title: "Create New Item",
+        errors: errors.array(),
+        categories: categories,
+      });
+    }
+
+    const { name, quantity, category_id } = req.body;
+    const new_category_id = parseInt(category_id);
+
+    let fileUrl;
+    if (req.file) {
+      const fileBuffer = req.file.buffer;
+      fileUrl = await uploadFile(fileBuffer);
+    }
+
+    const itemData = {
+      name: name,
+      quantity: quantity,
+      image_data: fileUrl,
+      category_id: new_category_id,
+    };
+
+    await db.createItem(itemData);
+
+    res.redirect(`/`);
+  },
+];
 
 async function updateGet(req, res) {
   const { id } = req.params;
@@ -62,22 +99,33 @@ async function updateGet(req, res) {
   });
 }
 
-async function updatePostRedirect(req, res) {
-  let fileUrl;
+const updatePostRedirect = [
+  validateItem,
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  if (req.file) {
-    const fileBuffer = req.file.buffer;
-    fileUrl = await uploadFile(fileBuffer);
-  } else {
-    fileUrl = req.body.old_image_data;
-  }
+    const item = { image_data: req.body.old_image_data, ...req.body };
 
-  const item = { image_data: fileUrl, ...req.body };
+    if (!errors.isEmpty()) {
+      const categories = await db.getAllCategories();
+      return res.status(400).render("update-item", {
+        title: "Update Item",
+        errors: errors.array(),
+        categories: categories,
+        item: item,
+      });
+    }
 
-  const queryString = `item=${encodeURIComponent(JSON.stringify(item))}`;
+    if (req.file) {
+      const fileBuffer = req.file.buffer;
+      item["image_data"] = await uploadFile(fileBuffer);
+    }
 
-  res.redirect(`/item_confirm/update/?${queryString}`);
-}
+    const queryString = `item=${encodeURIComponent(JSON.stringify(item))}`;
+
+    res.redirect(`/item_confirm/update/?${queryString}`);
+  },
+];
 
 async function updatePost(req, res) {
   const { id } = req.params;
